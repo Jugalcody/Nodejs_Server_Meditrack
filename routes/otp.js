6f950d36f2efde0d16319f5ad4f8b8d22e91c023
Jugalcody <jugalnerist@gmail.com>
2024-01-15 17:46:31 +0530

    modified a bit

--- routes/otp.js
+++ routes/otp.js
@@ -1,12 +1,13 @@
 const express=require('express');
 const router=express.Router();
 var sid='ACc5bc694c8f0a706090c13ae299818bbc';
-var token='5d0f066df0f706610fa7dba2cf7af715';
-const client=require('twilio')(sid,token);
+//var token='00501cc79e211e158fb6a251af6c5f32';
+
 
 router.post('/getotp',async (req,res)=>{
   
-    const{phone}=req.body;
+    const{token,phone}=req.body;
+    const client=require('twilio')(sid,token);
     
     try{
     var otp=Math.floor(1000+Math.random()*9000);
