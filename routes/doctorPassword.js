657b6f3c7aa33705c44097fa47e97998f6441d88
Jugalcody <jugalnerist@gmail.com>
2024-01-16 03:28:08 +0530

    fixed errors

---
+++ routes/doctorPassword.js
@@ -0,0 +1,31 @@
+const express=require('express');
+const router=express.Router();
+const Patient=require('../models/Doctor');
+const { Db } = require('mongodb');
+
+router.post('/getPassword',async (req,res)=>{
+    const {phone}=req.body;
+    try{
+    let patient_exist=await Patient.findOne({phone:phone});
+    if(patient_exist){
+       
+        res.json({
+            success:true,
+          password:patient_exist.password
+        });
+    }else{
+    res.json({
+        success:false,
+        msg:"failed"
+    })
+}
+    }
+    catch(err){
+        res.json({
+            success:false,
+            msg:"failed"
+        })
+    }
+})
+
+module.exports=router;
