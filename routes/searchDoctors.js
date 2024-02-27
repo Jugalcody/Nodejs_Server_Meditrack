db389e66ac14810685484f0aea8c151873a44282
Jugalcody <jugalnerist@gmail.com>
2024-02-27 16:32:50 +0530

    modified a bit

--- routes/searchDoctors.js
+++ routes/searchDoctors.js
@@ -8,16 +8,15 @@ router.post('/searchDoctors',async (req,res,next)=>{
     if(req.body.username) username=req.body.username;
     if(req.body.speciality) speciality=req.body.speciality;
     if(req.body.gender) gender=req.body.gender;
-    if(req.body.yoe) yoe=req.gender.yoe;
+    if(req.body.yoe) yoe=req.body.yoe;
     if(req.body.qualification) qualification=req.body.qualification;
     if(req.body.state) state=req.body.state;
     if(req.body.city) city=req.body.city;
 
-    
     try{
 
         
-            let doctor_exist=await Doctor.find({ username: { $regex:".*"+username+".*",$options:'i' },speciality: { $regex:".*"+speciality+".*",$options:'i' },yoe:{ $regex:".*"+yoe+".*",$options:'i' },gender:{ $regex:".*"+gender+".*",$options:'i' },qualification:{ $regex:".*"+qualification+".*",$options:'i' },state:{ $regex:".*"+state+".*",$options:'i' },city:{ $regex:".*"+city+".*",$options:'i' }});
+            let doctor_exist=await Doctor.find({ username: { $regex:".*"+username+".*",$options:'i' },speciality: { $regex:".*"+speciality+".*",$options:'i' },yoe:{ $regex:".*"+yoe+".*",$options:'i' },gender:{ $regex:gender+".*",$options:'i' },qualification:{ $regex:".*"+qualification+".*",$options:'i' },state:{ $regex:".*"+state+".*",$options:'i' },city:{ $regex:".*"+city+".*",$options:'i' }});
 
             if(doctor_exist.length>0){
     res.json({
