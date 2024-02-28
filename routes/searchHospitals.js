4be4611e1a95e9663f058f380a85a53d3b096b25
Jugalcody <jugalnerist@gmail.com>
2024-02-29 03:46:17 +0530

    modified a bit

--- routes/searchHospitals.js
+++ routes/searchHospitals.js
@@ -5,16 +5,17 @@ const { Db } = require('mongodb');
 
 router.post('/searchHospitals',async (req,res,next)=>{
 
-    var name="",type="",state="",city="",hospital_clinic="";
+    var name="",type="",state="",city="",hospital_clinic="",totalStar="";
     if(req.body.name) name=req.body.name;
     if(req.body.type) type=req.body.type;
     if(req.body.hospital_clinic) hospital_clinic=req.body.hospital_clinic;
     if(req.body.state) state=req.body.state;
     if(req.body.city) city=req.body.city;
+    if(req.body.totalStar) totalStar=req.body.totalStar;
 
     try{
     
-            let hospital_exist=await Doctor.find({ 'clinic_hospital.hospital_clinic':{ $regex:".*"+hospital_clinic+".*",$options:'i' },'clinic_hospital.name':{ $regex:".*"+name+".*",$options:'i' },'clinic_hospital.type':{ $regex:".*"+type+".*",$options:'i' },'clinic_hospital.state':{ $regex:".*"+state+".*",$options:'i' },'clinic_hospital.city':{ $regex:".*"+city+".*",$options:'i' }});
+            let hospital_exist=await Doctor.find({ 'clinic_hospital.hospital_clinic':{ $regex:".*"+hospital_clinic+".*",$options:'i' },'clinic_hospital.name':{ $regex:".*"+name+".*",$options:'i' },'clinic_hospital.type':{ $regex:".*"+type+".*",$options:'i' },'clinic_hospital.state':{ $regex:".*"+state+".*",$options:'i' },'clinic_hospital.city':{ $regex:".*"+city+".*",$options:'i' },'clinic_hospital.totalStar':{ $eq:totalStar }});
 
             if(hospital_exist.length>0){
     res.json({
